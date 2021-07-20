e9c7646793f29c1ec00902f152a4a0a1f8958a0d
ozzy castillo <oswaldo.castillo@revature.net>
2021-07-19 21:27:36 -0500

    LogginAspect

--- spring-boot-jdbc-app/src/main/java/com/ozzy/springbootjdbcapp/aspect/LoggingAspect.java
+++ spring-boot-jdbc-app/src/main/java/com/ozzy/springbootjdbcapp/aspect/LoggingAspect.java
@@ -1,11 +1,13 @@
 package com.ozzy.springbootjdbcapp.aspect;
 
-
-import org.aspectj.lang.annotation.Aspect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,4 +15,33 @@ public class LoggingAspect {
 
     private final Logger logger = LogManager.getLogger();
 
+    @Pointcut("within(com.ozzy.sprinbootjdbcapp..*)")
+    public void logAll(){};
+
+    @Before("logAll()")
+    public void logMethodStart(JoinPoint jp){
+        String methodSig = extractMethodSignature(jp);
+        String argStr = Arrays.toString(jp.getArgs());
+        logger.info("{} invot at {}", methodSig, LocalDate.now());
+        logger.info("Input arguments {}", argStr);
+    }
+
+    @AfterReturning(pointcut = "logAll()", returning = "returnedObj")
+    public void logMethodReturn(JoinPoint jp, Object returnedObj){
+        String methodSig = extractMethodSignature(jp);
+        logger.info("{} successfully returned at {}", methodSig, LocalDate.now());
+        logger.info("Object returned {}", returnedObj);
+    }
+
+    @AfterThrowing(pointcut = "logAll()", throwing = "e")
+    public void logMethodException(JoinPoint jp, Throwable e){
+        String methodSig = extractMethodSignature(jp);
+        logger.warn("{} was throw in method {} at {} with message {}", e.getClass().getSimpleName(), methodSig, LocalDate.now(), e.getMessage());
+    }
+
+    private String extractMethodSignature(JoinPoint jp){
+        return jp.getTarget().getClass().toString() + "." + jp.getSignature().getName();
+    }
+
 }
+
